76dbe196114c10d4c824f3b8a2c36befa6d8d031
kevwin717 <kevinn0308@gmail.com>
2025-02-19 16:29:11 -0600

    Save new jobs to MongoDB, prevent duplicates

--- jobs.js
+++ jobs.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Job = require('./models/Job')
 
 const fetchJobs = async () => {
     try {
@@ -16,7 +17,24 @@ const fetchJobs = async () => {
         );
         
         const jobs = response.data.results;
-        console.log(jobs);
+
+        for(const jobData of jobs) {
+            const existingJob = await Job.findOne({ jobId: jobData.id });
+            if(!existingJob) {
+                const job = new Job({
+                    jobId: jobData.id,
+                    title: jobData.title,
+                    company: jobData.company.display_name,
+                    location: jobData.location.display_name,
+                    description: jobData.description,
+                    url: jobData.redirect_url,
+                });
+                await job.save();
+                console.log('Saved job: ${job.title}');
+            } else {
+                console.log('Job already exists: ${existingJob.title}');
+            }
+        }
     } catch (error) {
         console.error('Error fetching jobs:', error.message);
     }
